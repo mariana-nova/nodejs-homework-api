a60785ac8de70c8edf767de6faeac53285cdc870
mariana-nova <119463538+mariana-nova@users.noreply.github.com>
2023-11-08 21:02:50 -0500

    2 cambio

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,32 @@
 const express = require('express')
+const models = require ('../../models/contacts')
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const data = await models.listContacts()
+  
+  res.status(200).json(data)
 })
 
+
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const id = req.params.contactId;
+  const data = await models.getContactById(id)
+  res.status(200).json(data)
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const {name,email,phone} = req.body;
+  const newContacts ={
+    id : 123,
+    name,
+    email,
+    phone,
+  };
+  const data = await models.addContact(newContacts);
+
+  res.status(201).json(data)
 })
 
 router.delete('/:contactId', async (req, res, next) => {
